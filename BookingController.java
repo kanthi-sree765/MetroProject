dace0f9929d7ae6741e11528d14ab629fbdd2b7d
kanthi-sree765 <gkanthisreeyadav@gmail.com>
2025-09-22 13:12:19 +0530

    Added Booking Controller.java

---
+++ BookingController.java
@@ -0,0 +1,6 @@
+public class BookingCollector {
+    public void collectBooking() {
+        System.out.println("Collect booking from main branch");
+        System.out.println("Collect booking with payment integration");
+    }
+}
